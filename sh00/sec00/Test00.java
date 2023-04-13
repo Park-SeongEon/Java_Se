4c6b9328d798fd8ba74396b59d0ebcff8da34009
one031411 <one031411@naver.com>
2023-04-13 11:40:21 +0900

    덧셈 연습

--- sh00/sec00/Test00.java
+++ sh00/sec00/Test00.java
@@ -3,7 +3,7 @@ package sh00.sec00;
 public class Test00 {
 
 	public static void main(String[] args) {
-		// 
+		// 문자열 출력
 		System.out.println("Hello World");
 
 	}
---
+++ sh01/sec00/Test01.java
@@ -0,0 +1,14 @@
+package sh01.sec00;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		
+		int a=5;
+		int b=3;
+		
+		System.out.println("a+b=" + (a+b));
+
+	}
+
+}
