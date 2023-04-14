afe92c909178251cf0246d5e605072a9f7cb6f1d
one031411 <one031411@naver.com>
2023-04-14 17:00:03 +0900

    데이터변환과변수의범위

---
+++ ch02/sec06/Test01.java
@@ -0,0 +1,18 @@
+package ch02.sec06;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 3;
+		
+		double c = 0;
+		
+		c = (float)a/b;
+		
+		System.out.printf("%.2f",c);
+
+	}
+
+}
