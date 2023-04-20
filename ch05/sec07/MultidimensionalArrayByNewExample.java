6126d0add228fb17b916ca093af072afb91013f3
one031411 <one031411@naver.com>
2023-04-20 16:42:25 +0900

    배열

--- ch05/sec07/MultidimensionalArrayByNewExample.java
+++ ch05/sec07/MultidimensionalArrayByNewExample.java
@@ -7,7 +7,8 @@ public class MultidimensionalArrayByNewExample {
 		int[][] mathScores = new int[2][3];
 		//배열 항목 초기값 출력
 		for (int i = 0; i < mathScores.length; i++) {   //반의 수만큼 반복
-			for (int k = 0; k < mathScores[i].length; k++) {//해당 반의 학생 수만큼 반복
+			for (int k = 0; k < mathScores
+					[i].length; k++) {//해당 반의 학생 수만큼 반복
 				
 				System.out.println("mathScores[" + i + "][" + k + "]: " + mathScores[i][k]);
 				}
---
+++ ch05/sec07/Test00.java
@@ -0,0 +1,10 @@
+package ch05.sec07;
+
+public class Test00 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+
+	}
+
+}
